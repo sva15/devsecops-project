74f0972cd49225bdf15d01366338ab4136d346dc
sva15 <85981504+sva15@users.noreply.github.com>
2024-12-25 19:31:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
